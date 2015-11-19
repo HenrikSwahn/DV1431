cb2ffca51565b50b6fda87a202330c8092a3b1a3
Dino Opijac <dino.opijac@gmail.com>
2015-11-19 13:57:04 +0100

    added storyboard for SearchEntryTableViewController

--- Collector/SearchTableViewController.swift
+++ Collector/SearchTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@IBDesignable
 class SearchTableViewController: UITableViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     // MARK: - Variables and constances
