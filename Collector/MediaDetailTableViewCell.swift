a5588014b60f8608552f4a7b29a46e707c8ac9a0
Dino Opijac <dino.opijac@gmail.com>
2015-11-26 22:35:38 +0100

    MediaDetail* uses dominant colors

--- Collector/MediaDetailTableViewCell.swift
+++ Collector/MediaDetailTableViewCell.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class MediaDetailTableViewCell: UITableViewCell {
+    @IBOutlet weak var iconView: UIIconImageView!
     @IBOutlet weak var keyLabel: UILabel!
     @IBOutlet weak var valueLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        
         // Initialization code
     }
 
@@ -22,5 +24,32 @@ class MediaDetailTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    private func setImage(named name: String) {
+        iconView.setImage(named: name, colored: valueLabel.textColor)
+    }
+    
+    private var shadow: UIColor? {
+        willSet {
+            let shadowLayer = CAGradientLayer()
+            shadowLayer.frame = CGRectMake(0, 0, frame.width, 2)
+            shadowLayer.colors = [newValue!.CGColor, UIColor.clearColor().CGColor]
+            shadowLayer.opacity = 0.5
+            contentView.layer.addSublayer(shadowLayer)
+        }
+    }
+    
+    func setDominantColors(with colors: UIImageColors?, indexPath: NSIndexPath) {
+        if let color = colors {
+            backgroundColor = color.secondaryColor
+            contentView.backgroundColor = color.secondaryColor
+            keyLabel.textColor = color.detailColor
+            valueLabel.textColor = color.primaryColor.isDarkColor ? color.backgroundColor : color.primaryColor
+            iconView.setRenderingMode(withColor: keyLabel.textColor)
+        
+            if indexPath.row == 0 {
+                shadow = color.secondaryColor
+            }
+        }
+    }
 }
--- Collector/MediaDetailViewController.swift
+++ Collector/MediaDetailViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+    // MARK: - Coloring
+    private var colors: UIImageColors?
+    
     // MARK: - Mockup members
     private var mediaModel = [(String, String)]()
     
@@ -16,10 +19,17 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
         static let mediaDetailTableCellIdentifier = "media detail cell id"
     }
     
+    private struct Color {
+        static let black = UIColor.blackColor().CGColor
+        static let clear = UIColor.clearColor().CGColor
+    }
+    
+    var headerView: UIView!
+    @IBOutlet weak var headerImageView: UIImageView!
     @IBOutlet weak var backgroundView: UIImageView!
     @IBOutlet weak var coverImageView: UIImageView!
-    @IBOutlet weak var ratingView: UIRatingView!
     
+    @IBOutlet weak var visualView: UIVisualEffectView!
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var titleLabel: UIMarqueeLabel!
     @IBOutlet weak var runtimeLabel: UILabel!
@@ -27,17 +37,6 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
     
     @IBOutlet weak var ownerLocationLabel: UILabel!
     @IBOutlet weak var ownerTypeLabel: UILabel!
-    
-    /*@IBOutlet weak var imageView: UIImageView!
-
-    @IBOutlet weak var titleLabel: UIMarqueeLabel!
-    @IBOutlet weak var ratingView: UIRatingView!*/
-    
-    private struct Color {
-        static let black = UIColor.blackColor().CGColor
-        static let clear = UIColor.clearColor().CGColor
-    }
-    
     @IBOutlet weak var tableView: UITableView! {
         didSet {
             tableView.backgroundColor = UIColor.clearColor()
@@ -45,13 +44,32 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     @IBOutlet weak var testimg: UIIconImageView!
+
+    private func updateColor() {
+        if let c = colors {
+            let color = c.primaryColor.isDarkColor ? c.primaryColor : c.backgroundColor
+
+            self.titleLabel.textColor = color
+            self.yearLabel.textColor = color
+            self.runtimeLabel.textColor = color
+            self.genreLabel.textColor = color
+            self.ownerLocationLabel.textColor = c.backgroundColor
+            self.ownerTypeLabel.textColor = c.backgroundColor
+            
+            // Separators (borders)
+            self.tableView.separatorColor = c.detailColor
+        }
+    }
+
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-/*        let img = testimg.image?.imageWithRenderingMode(.AlwaysTemplate)
-        testimg.image = img*/
+    
+        // Get the dominantcolor from this image
+        let size = self.coverImageView.frame.size
+        self.colors = self.coverImageView.image?.getColors(size)
+        self.updateColor()
         
         // Set self as delegate and datasource
         tableView.delegate = self
@@ -76,18 +94,40 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
         self.titleLabel.type = .LeftRight
         self.titleLabel.scrollRate = 100.0
         self.titleLabel.fadeLength = 20.0
+        
+        // Sets the background image to the cover image
+        self.backgroundView.image = self.coverImageView.image
+        self.headerImageView.image = self.coverImageView.image
+        
+        h = tableView.tableHeaderView?.frame.size.height
+        headerView = tableView.tableHeaderView
+        tableView.tableHeaderView = nil
+        tableView.addSubview(headerView)
+        
+        tableView.contentInset = UIEdgeInsets(top: h!, left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -h!)
+        updateHeaderView()
+    }
+    
+    private var h: CGFloat?
+    
+    func updateHeaderView() {
+        var rect = CGRect(x: 0, y: -h!, width: tableView.bounds.width, height: h!)
+        if tableView.contentOffset.y < -h! {
+            rect.origin.y = tableView.contentOffset.y
+            rect.size.height = -tableView.contentOffset.y
+        }
+        
+        headerView.frame = rect
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        updateHeaderView()
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
-        self.shadowLayerOnCover(coverImageView.layer)
-        
-        let gradient = CAGradientLayer()
-        gradient.frame = backgroundView.bounds
-        gradient.colors = [Color.clear, Color.black, Color.clear]
-        gradient.opacity = 0.5
-        backgroundView.layer.insertSublayer(gradient, atIndex: 0)
+        coverImageView.dropShadow()
     }
 
     override func didReceiveMemoryWarning() {
@@ -95,18 +135,16 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
         // Dispose of any resources that can be recreated.
     }
     
-    private func shadowLayerOnCover(layer: CALayer) {
-        layer.shadowColor = Color.black
-        layer.shadowOffset = CGSizeMake(0, 0)
-        layer.shadowOpacity = 1
-        layer.shadowRadius = 1.0
-    }
-    
     // MARK: - TableView delegates
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.mediaDetailTableCellIdentifier) as! MediaDetailTableViewCell
+
         cell.keyLabel?.text = mediaModel[indexPath.row].0
         cell.valueLabel?.text = mediaModel[indexPath.row].1
+        
+        // Set the proper background color
+        cell.setDominantColors(with: colors, indexPath: indexPath)
+        
         return cell
     
     }
@@ -123,5 +161,16 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+private extension UIImageView {
+    func dropShadow(colored: UIColor? = nil, offset: CGSize = CGSizeMake(0,0), opacity: Float = 1, radius: CGFloat = 1.0) {
+        if let color = colored {
+                layer.shadowColor = color.CGColor
+        }
+        
+        layer.shadowOffset = offset
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+    }
 }
