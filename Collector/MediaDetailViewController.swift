9759d6a0076583167b1d93d34255dbf7ee9ca166
Dino Opijac <dino.opijac@gmail.com>
2015-11-30 17:47:40 +0100

    using reusable UIStretchableTableView

--- Collector/MediaDetailViewController.swift
+++ Collector/MediaDetailViewController.swift
@@ -8,43 +8,49 @@
 
 import UIKit
 
-class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
-    // MARK: - Coloring
+class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // MARK: - Private Members
     private var colors: UIImageColors?
-    
-    // MARK: - Mockup members
-    private var mediaModel = [(String, String)]()
-    
+    private var media = [(String, String)]()
     private struct Storyboard {
         static let mediaDetailTableCellIdentifier = "media detail cell id"
     }
     
-    private struct Color {
-        static let black = UIColor.blackColor().CGColor
-        static let clear = UIColor.clearColor().CGColor
-    }
-    
-    var headerView: UIView!
     @IBOutlet weak var headerImageView: UIImageView!
-    @IBOutlet weak var backgroundView: UIImageView!
     @IBOutlet weak var coverImageView: UIImageView!
-    
-    @IBOutlet weak var visualView: UIVisualEffectView!
+
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var titleLabel: UIMarqueeLabel!
     @IBOutlet weak var runtimeLabel: UILabel!
     @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var ratingView: UIRatingView!
     
     @IBOutlet weak var ownerLocationLabel: UILabel!
     @IBOutlet weak var ownerTypeLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView! {
+    @IBOutlet weak var tableView: UIStretchableTableView! {
         didSet {
+            // The tableView will not conform to Storyboard Clear color property
+            // it has to be set in code.
             tableView.backgroundColor = UIColor.clearColor()
         }
     }
-    
-    @IBOutlet weak var testimg: UIIconImageView!
 
+    // MARK: - Mock Data Initialization 
+    private func setMockData() {
+        // Data for labels
+        self.yearLabel.text = "2007"
+        self.titleLabel.text = "The Lord of The Rings: Fellowship of the Ring"
+        self.runtimeLabel.text = "1 hour, 40 minutes"
+        self.genreLabel.text = "Drama, Sci-Fi, Thriller"
+        self.ownerLocationLabel.text = "Physical"
+        self.ownerTypeLabel.text = "Blu-Ray"
+        
+        // Data for the table
+        media.append(("Description", "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure."))
+        media.append(("Writers", "Mark Protocevic, Akiva Goldsman"))
+        media.append(("Actors", "Will Smith, Alice Braga, Charlie Tahan"))
+    }
+    
     private func updateColor() {
         if let c = colors {
             let color = c.primaryColor.isDarkColor ? c.primaryColor : c.backgroundColor
@@ -56,8 +62,8 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
             self.ownerLocationLabel.textColor = c.backgroundColor
             self.ownerTypeLabel.textColor = c.backgroundColor
             
-            // Separators (borders)
-            self.tableView.separatorColor = c.detailColor
+            // Background color
+            self.view.backgroundColor = c.secondaryColor
         }
     }
 
@@ -65,82 +71,56 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Get the dominantcolor from this image
-        let size = self.coverImageView.frame.size
-        self.colors = self.coverImageView.image?.getColors(size)
-        self.updateColor()
         
         // Set self as delegate and datasource
         tableView.delegate = self
         tableView.dataSource = self
         
+        // Make table cells resizable
         tableView.estimatedRowHeight = tableView.rowHeight
         tableView.rowHeight = UITableViewAutomaticDimension
         
-        mediaModel.append(("Description", "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure."))
-        mediaModel.append(("Writers", "Mark Protocevic, Akiva Goldsman"))
-        mediaModel.append(("Actors", "Will Smith, Alice Braga, Charlie Tahan"))
+        // Set mock data
+        self.setMockData()
         
-        // Mock data for the movie
-        self.titleLabel.text = "The Lord of The Rings: Fellowship of the Ring"
-        self.yearLabel.text = "2007"
-        self.runtimeLabel.text = "1 hour, 40 minutes"
-        self.genreLabel.text = "Drama, Sci-Fi, Thriller"
-        self.ownerLocationLabel.text = "Physical"
-        self.ownerTypeLabel.text = "Blu-Ray"
+        // Sets the background image to the cover image
+        self.headerImageView.image = self.coverImageView.image
+        
+        // Drop a shadow around the cover image
+        coverImageView.dropShadow()
+        
+        // Get the dominant color from the cover image
+        let size = self.coverImageView.frame.size
+        self.colors = self.coverImageView.image?.getColors(size)
+        self.updateColor()
         
         // Do any additional setup after loading the view.
         self.titleLabel.type = .LeftRight
         self.titleLabel.scrollRate = 100.0
         self.titleLabel.fadeLength = 20.0
         
-        // Sets the background image to the cover image
-        self.backgroundView.image = self.coverImageView.image
-        self.headerImageView.image = self.coverImageView.image
-        
-        h = tableView.tableHeaderView?.frame.size.height
-        headerView = tableView.tableHeaderView
-        tableView.tableHeaderView = nil
-        tableView.addSubview(headerView)
-        
-        tableView.contentInset = UIEdgeInsets(top: h!, left: 0, bottom: 0, right: 0)
-        tableView.contentOffset = CGPoint(x: 0, y: -h!)
-        updateHeaderView()
-    }
-    
-    private var h: CGFloat?
-    
-    func updateHeaderView() {
-        var rect = CGRect(x: 0, y: -h!, width: tableView.bounds.width, height: h!)
-        if tableView.contentOffset.y < -h! {
-            rect.origin.y = tableView.contentOffset.y
-            rect.size.height = -tableView.contentOffset.y
-        }
-        
-        headerView.frame = rect
-    }
-    
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        updateHeaderView()
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        coverImageView.dropShadow()
-    }
 
+        
+        // Hide empty table cells
+        self.hideEmptyTableViewCells()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - TableView delegates
+    
+    // MARK: - TableView
+    private func hideEmptyTableViewCells() {
+        self.tableView.tableFooterView = UIView()
+    }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.mediaDetailTableCellIdentifier) as! MediaDetailTableViewCell
 
-        cell.keyLabel?.text = mediaModel[indexPath.row].0
-        cell.valueLabel?.text = mediaModel[indexPath.row].1
+        cell.keyLabel?.text = media[indexPath.row].0
+        cell.valueLabel?.text = media[indexPath.row].1
         
         // Set the proper background color
         cell.setDominantColors(with: colors, indexPath: indexPath)
@@ -150,11 +130,11 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mediaModel.count
+        return media.count
     }
+    
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
@@ -163,12 +143,12 @@ class MediaDetailViewController: UIViewController, UITableViewDelegate, UITableV
     */
 }
 
+// Drops an shadow around a UIImageView
 private extension UIImageView {
     func dropShadow(colored: UIColor? = nil, offset: CGSize = CGSizeMake(0,0), opacity: Float = 1, radius: CGFloat = 1.0) {
-        if let color = colored {
-                layer.shadowColor = color.CGColor
-        }
-        
+        let color = (colored != nil) ? colored!.CGColor : UIColor.blackColor().CGColor
+    
+        layer.shadowColor = color
         layer.shadowOffset = offset
         layer.shadowOpacity = opacity
         layer.shadowRadius = radius
