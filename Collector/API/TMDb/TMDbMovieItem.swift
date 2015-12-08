ec4e0a51b9fa7b39f5aa587717aa30b8d61a4cd3
Dino Opijac <dino.opijac@gmail.com>
2015-12-08 20:04:07 +0100

    TMDb reformats dates and URLs

--- Collector/API/TMDb/TMDbMovieItem.swift
+++ Collector/API/TMDb/TMDbMovieItem.swift
@@ -1,9 +1,17 @@
+//
+//  TMDbConfigurationResource.swift
+//  Collector
+//
+//  Created by Dino Opijac on 05/12/15.
+//  Copyright © 2015 Dino Opijac. All rights reserved.
+//
+
 import Foundation
 
 public struct TMDbMovieItem {
     var id: Int
     var image: String
-    var release_date: String
+    var release: String
     var language: String
     var title: String
     var synopsis: String
