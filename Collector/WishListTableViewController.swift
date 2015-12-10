6696ba57cb7589f80442d39483c60f0a1de60b57
Dino Opijac <dino.opijac@gmail.com>
2015-12-10 16:30:08 +0100

    moved wishlist item to own file

--- Collector/WishListTableViewController.swift
+++ Collector/WishListTableViewController.swift
@@ -8,50 +8,6 @@
 
 import UIKit
 
-enum MediaType: String {
-    case Movie  = "Movie"
-    case Music  = "Music"
-    case Game   = "Game"
-    case Book   = "Book"
-}
-
-class WishListItem {
-    // ID for the item in CoreData?
-    var id: Int
-    
-    // ID for the item in the API
-    var aid: String
-    
-    // Type of media
-    var type: MediaType
-    
-    // The cover art for the media
-    var imageData: UIImage?
-    
-    // Title for the item
-    // Movie: This is should contain the title of the movie
-    // Album: This is should contain the title of the album
-    // Game: This should contain then name of the game
-    // Book: This should contain the title of the book
-    var title: String
-    
-    // Detail
-    // Movie: This should be empty (optional)
-    // Album: This should contain the artist
-    // Game: This should be empty
-    // Book: This should contain author
-    var detail: String?
-    
-    init(id: Int, aid: String, type: MediaType, imageData: UIImage?, title: String, detail: String?) {
-        self.id = id
-        self.aid = aid
-        self.type = type
-        self.imageData = imageData
-        self.title = title
-        self.detail = detail
-    }
-}
-
 class WishListTableViewController: UITableViewController {
     private var model: [WishListItem]?
     
