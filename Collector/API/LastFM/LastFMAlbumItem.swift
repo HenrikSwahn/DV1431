0e6cd410a2a1a861efaeb130cc0384207614f0c1
Dino Opijac <dino.opijac@gmail.com>
2015-12-10 13:36:30 +0100

    remove lastfm api - not used

--- Collector/API/LastFM/LastFMAlbumItem.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct LastFMAlbumItem {
-    var id: String
-    var name: String
-    var artist: String
-    var image: String?
-    var tracks: [LastFMAlbumTrackItem]?
-}
