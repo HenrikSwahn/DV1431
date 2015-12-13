9bdbab4971c626527570ad5e94f4aacb631a3eac
Dino Opijac <dino.opijac@gmail.com>
2015-12-13 15:29:02 +0100

    reusable classes for k/v

---
+++ Collector/GeneralDetailTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  GeneralDetailTableViewCell.swift
+//  Collector
+//
+//  Created by Dino Opijac on 13/12/15.
+//  Copyright © 2015 Dino Opijac. All rights reserved.
+//
+
+import Foundation
---
+++ Collector/KeyValueAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  KeyValueAdapter.swift
+//  Collector
+//
+//  Created by Dino Opijac on 13/12/15.
+//  Copyright © 2015 Dino Opijac. All rights reserved.
+//
+
+import Foundation
